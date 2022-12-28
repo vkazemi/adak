849836d80ef22cf939479e45314f88243b88af2d
Vahid Kazemi <v_kazemi@outlook.com>
2022-12-28 12:06:30 +0330

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,20 +1,20 @@
 <?php
 
 //DB params
-define('DB_HOST', 'localhost');
-define('DB_USER','dev_bot');
-define('DB_PASSWORD', 'dev%bot');
-define('DB_NAME', 'nafispanel');
+define('DB_HOST', '');
+define('DB_USER','');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
 //App root
 define('APPROOT', dirname(dirname(__FILE__)));
 // Asset root
 define('ASSETROOT', dirname(dirname(dirname(__FILE__))));
 //Url root
-define('URLROOT','http://localhost/nafispanel');
+define('URLROOT','');
 
 //Site name
-define('SITENAME','NafisPanel');
+define('SITENAME','');
 //App version
-define('APPVERSION','1.0.0');
+define('APPVERSION','');
 ?>
